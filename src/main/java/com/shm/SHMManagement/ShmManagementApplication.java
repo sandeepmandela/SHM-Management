ab9c110e415ebd6c44f0d7147cc2fdce14c0ce42
sandeep mandela <44814865+sandeepmandela@users.noreply.github.com>
2023-08-08 19:22:59 -0500

    Add files via upload

---
+++ src/main/java/com/shm/SHMManagement/ShmManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shm.SHMManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShmManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShmManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shm/SHMManagement/ShmManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shm.SHMManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShmManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
